5556d249650ef9800b4897f4ae2d0e0d50369922
priscilaandreani <priscilaandreani@gmail.com>
2025-03-31 18:07:44 -0300

    feat: refactor meowing logic in cat.c to use a separate function and for loop

--- lecture-1/cat.c
+++ lecture-1/cat.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
+void meow(void){
+  printf("Meow!\n");
+}
+
 int main(void)
 {
-  int i = 0;
-  while (i < 3)
-  {
-    printf("Meow!\n");
-    i++;
-  }
-
   for(int i = 0; i < 3; i++)
   {
-    printf("Meow with for!\n");
+    meow();
   }
 }
