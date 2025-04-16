4466c1e9ae799a6433055ed3b416df79a1c1888e
Priscila Andreani <priscila.andreani@grupoboticario.com.br>
2025-04-16 20:39:55 -0300

    refactor: isolate logic to a new function

--- lecture-1/cat3.c
+++ lecture-1/cat3.c
@@ -2,22 +2,29 @@
 #include <stdio.h>
 
 void meow(int n);
+int get_positive_int(void);
 
 int main(void)
 {
-    int n;
-    do
-    {
-        n = get_int("Number: ");
-    }
-    while (n < 1);
-    meow(n);
+  int times = get_positive_int();
+  meow(times);
 }
 
 void meow(int n)
 {
-    for (int i = 0; i<n; i++)
-    {
-        printf("meow\n");
-    }
+  for (int i = 0; i<n; i++)
+  {
+    printf("meow\n");
+  }
+}
+
+int get_positive_int(void)
+{
+  int n;
+  do
+  {
+      n = get_int("Number: ");
+  }
+  while (n < 1);
+  return n;
 }
