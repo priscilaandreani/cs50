d20117719825ccde8fe5a24129bdee96afa6e47b
Priscila Andreani <priscila.andreani@grupoboticario.com.br>
2025-04-16 20:32:07 -0300

    feat: add cat3.c with meow function to print 'meow' n times based on user input using do while

---
+++ lecture-1/cat3.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void meow(int n);
+
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Number: ");
+    }
+    while (n < 1);
+    meow(n);
+}
+
+void meow(int n)
+{
+    for (int i = 0; i<n; i++)
+    {
+        printf("meow\n");
+    }
+}
